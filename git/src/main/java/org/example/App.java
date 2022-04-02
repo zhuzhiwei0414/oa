baf4e70d3d9e8bab2f8026fb8c9881a0471c0c4f
git_awei <zhuzhiwei0414@163.com>
2022-04-02 09:25:12 +0800

    3 commit

--- git/src/main/java/org/example/App.java
+++ git/src/main/java/org/example/App.java
@@ -9,8 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! 123" );
         System.out.println( "Hello World 朱志威!" );
         System.out.println( "Hello World 我是帅B!" );
         System.out.println( "Hello World 我裂开了!" );
@@ -18,5 +17,6 @@ public class App
         System.out.println( "Hello World 岗林小老弟" );
         System.out.println( "Hello World 我是弟弟" );
         System.out.println("无语了");
+
     }
 }
