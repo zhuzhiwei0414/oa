cf7611843ae7c909f8b7b2f4d8571cf1fb5bb6ab
zhuzhiwei0414 <102655646+zhuzhiwei0414@users.noreply.github.com>
2022-04-02 08:41:56 +0800

    2 commit

--- git/src/main/java/org/example/App.java
+++ git/src/main/java/org/example/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!" );
+        System.out.println( "Hello World 朱志威!" );
+        System.out.println( "Hello World 我是帅B!" );
     }
 }
