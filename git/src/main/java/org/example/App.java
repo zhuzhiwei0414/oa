2bce412802d9be2115e1f5130a92228dd90f7720
zhuzhiwei0414 <102655646+zhuzhiwei0414@users.noreply.github.com>
2022-04-02 09:08:45 +0800

    4 commit

--- git/src/main/java/org/example/App.java
+++ git/src/main/java/org/example/App.java
@@ -13,9 +13,7 @@ public class App
         System.out.println( "Hello World 朱志威!" );
         System.out.println( "Hello World 我是帅B!" );
         System.out.println( "Hello World 我裂开了!" );
-        System.out.println( "Hello World 我裂开了!" );
-        System.out.println( "Hello World 我裂开了!" );
         System.out.println( "Hello World 岗林小老弟" );
-        System.out.println( "Hello World 岗林小老弟" );
+        System.out.println( "Hello World 我是弟弟" );
     }
 }
