c933333238637c6a718fb2497471023da53938cb
git_awei <zhuzhiwei0414@163.com>
2022-04-02 09:28:00 +0800

    3 commit

--- git/src/main/java/org/example/App.java
+++ git/src/main/java/org/example/App.java
@@ -10,6 +10,8 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println( "Hello World! 123" );
+        System.out.println( "Hello World! 2222222" );
+        System.out.println( "Hello World! 3333333333333" );
         System.out.println( "Hello World 朱志威!" );
         System.out.println( "Hello World 我是帅B!" );
         System.out.println( "Hello World 我裂开了!" );
